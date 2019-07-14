19db449eee7fcf010eaf1ec3ebe1deec57ad1ae4
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-14 02:10:46 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -22,10 +22,9 @@ function destructivelyAddElementToEndOfArray(array, element){
 
 function accessElementInArray(array, index){
   return array[2]
-  
 }
 
 function destructivelyRemoveElementsFromBeginningOfArray(array){
-  
+  array.shift()
   return array
 }
