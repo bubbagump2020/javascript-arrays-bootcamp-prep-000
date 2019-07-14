c19cfc5735e273254beeafb52e3224d3ffb67359
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-14 01:59:39 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,12 +1,13 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 var args = ['foo', 1]
 function addElementToBeginningOfArray(array, element){
-  array.unshift(element)
+  array.push(element)
   return array
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  
+  array.unshift(element)
+  return array
 }
 
 function addElementToEndOfArray(array, element){
